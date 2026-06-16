ArianK16a/android_kernel_fairphone_sm7635-modules

qcom: video-driver: Fix header includes
Change-Id: I67e3dfa8c3286effbe26c30b791af46a7a8b8020
Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

qcom/opensource/video-driver/driver/platform/common/src/msm_vidc_platform.c
@@ -27,9 +27,9 @@
 #include "msm_vidc_iris2.h"
 #endif
 #if defined(CONFIG_MSM_VIDC_PINEAPPLE)
-#include "msm_vidc_pineapple.h"
-#include "msm_vidc_cliffs.h"
-#include "msm_vidc_iris33.h"
+#include "../../pineapple/inc/msm_vidc_pineapple.h"
+#include "../../cliffs/inc/msm_vidc_cliffs.h"
+#include "../../../variant/iris33/inc/msm_vidc_iris33.h"
 #endif
 #if defined(CONFIG_MSM_VIDC_KALAMA)
 #include "msm_vidc_kalama.h"

Niyush-04/spring_modules

qcom/opensource/video-driver/driver/platform/common/src/msm_vidc_platform.c
@@ -37,7 +37,7 @@
 #include "msm_vidc_ravelin.h"
 #endif
 #if defined(CONFIG_MSM_VIDC_BLAIR)
-#include "msm_vidc_blair.h"
+#include "../../blair/inc/msm_vidc_blair.h"
 #endif
 #if defined(CONFIG_MSM_VIDC_NEO)
 #include "msm_vidc_neo.h"